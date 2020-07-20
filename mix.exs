76715e80572f97a435b634bb413d20bfbdfd6ff1
Marcus Kruse <esurk.sucram@gmail.com>
2020-07-20 19:27:03 +0200

    Fix docs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tox.MixProject do
   def project do
     [
       app: :tox,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
