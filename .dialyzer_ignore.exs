e54a85941ef9406fd28853b2b320150342b3f2bd
Marcus Kruse <esurk.sucram@gmail.com>
2021-09-23 18:03:53 +0200

    Update .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,8 @@
 [
+  {"lib/tox/datetime.ex", :pattern_match, 587},
   {"lib/tox/datetime.ex", :pattern_match, 592},
   {"lib/tox/datetime.ex", :pattern_match, 595},
+  {"lib/tox/datetime.ex", :pattern_match, 745},
   {"lib/tox/datetime.ex", :pattern_match, 750},
   {"lib/tox/datetime.ex", :pattern_match, 753}
 ]
