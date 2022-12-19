b75b69d7728e66bdc8237e4e0ac8d52f73e5e9f1
Marcus Kruse <esurk.sucram@gmail.com>
2022-12-19 07:42:32 +0100

    Set repuired Elixir version to 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tox.MixProject do
     [
       app: :tox,
       version: "0.2.3",
-      elixir: "~> 1.8",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
