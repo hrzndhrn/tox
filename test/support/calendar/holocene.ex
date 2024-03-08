d0c028934b7aa920993141df286d4804e8e23468
Marcus Kruse <esurk.sucram@gmail.com>
2024-03-08 16:22:46 +0100

    Update holocene calendar

--- test/support/calendar/holocene.ex
+++ test/support/calendar/holocene.ex
@@ -22,18 +22,15 @@ defmodule Calendar.Holocene do
     }
   end
 
-  @impl true
   def date_to_string(year, month, day) do
     "#{year}-#{zero_pad(month, 2)}-#{zero_pad(day, 2)}"
   end
 
-  @impl true
   def naive_datetime_to_string(year, month, day, hour, minute, second, microsecond) do
     "#{year}-#{zero_pad(month, 2)}-#{zero_pad(day, 2)} " <>
       Calendar.ISO.time_to_string(hour, minute, second, microsecond)
   end
 
-  @impl true
   def datetime_to_string(
         year,
         month,
@@ -56,13 +53,10 @@ defmodule Calendar.Holocene do
   defp zone_to_string(_, _, _, "Etc/UTC"), do: ""
   defp zone_to_string(_, _, abbr, zone), do: " " <> abbr <> " " <> zone
 
-  @impl true
   defdelegate time_to_string(hour, minute, second, microsecond), to: Calendar.ISO
 
-  @impl true
   def day_rollover_relative_to_midnight_utc(), do: {0, 1}
 
-  @impl true
   def naive_datetime_from_iso_days(entry) do
     {year, month, day, hour, minute, second, microsecond} =
       Calendar.ISO.naive_datetime_from_iso_days(entry)
@@ -70,7 +64,6 @@ defmodule Calendar.Holocene do
     {year + 10_000, month, day, hour, minute, second, microsecond}
   end
 
-  @impl true
   def naive_datetime_to_iso_days(year, month, day, hour, minute, second, microsecond) do
     Calendar.ISO.naive_datetime_to_iso_days(
       year - 10_000,
@@ -91,7 +84,6 @@ defmodule Calendar.Holocene do
     "-" <> zero_pad(-val, count)
   end
 
-  @impl true
   def parse_date(string) do
     {year, month, day} =
       string
@@ -106,76 +98,63 @@ defmodule Calendar.Holocene do
     end
   end
 
-  @impl true
   def valid_date?(year, month, day) do
     :calendar.valid_date(year, month, day)
   end
 
-  @impl true
   def months_in_year(year) do
     Calendar.ISO.months_in_year(year - 10_000)
   end
 
-  @impl true
   def days_in_month(year, month) do
     Calendar.ISO.days_in_month(year - 10_000, month)
   end
 
-  @impl true
   def leap_year?(year) do
     Calendar.ISO.leap_year?(year - 10_000)
   end
 
-  @impl true
   def day_of_week(year, month, day, starting_on) do
     Calendar.ISO.day_of_week(year - 10_000, month, day, starting_on)
   end
 
-  @impl true
   def day_of_year(year, month, day) do
     Calendar.ISO.day_of_year(year - 10_000, month, day)
   end
 
-  @impl true
   def quarter_of_year(year, month, day) do
     Calendar.ISO.quarter_of_year(year - 10_000, month, day)
   end
 
+  def year_of_era(year) do
+    Calendar.ISO.year_of_era(year - 10_000)
+  end
+
   if {:year_of_era, 3} in Calendar.ISO.__info__(:functions) do
-    @impl true
     def year_of_era(year, month, day) do
       Calendar.ISO.year_of_era(year - 10_000, month, day)
     end
   end
 
-  @impl true
   def day_of_era(year, month, day) do
     Calendar.ISO.day_of_era(year - 10_000, month, day)
   end
 
-  @impl true
   defdelegate parse_time(string), to: Calendar.ISO
 
-  @impl true
   defdelegate parse_naive_datetime(string), to: Calendar.ISO
 
-  @impl true
   defdelegate parse_utc_datetime(string), to: Calendar.ISO
 
-  @impl true
   defdelegate time_from_day_fraction(day_fraction), to: Calendar.ISO
 
-  @impl true
   defdelegate time_to_day_fraction(hour, minute, second, microsecond), to: Calendar.ISO
 
-  @impl true
   defdelegate valid_time?(hour, minute, second, microsecond), to: Calendar.ISO
 
   if {:iso_days_to_end_of_day, 1} in Calendar.ISO.__info__(:functions) do
-    @impl true
     defdelegate iso_days_to_beginning_of_day(iso_days), to: Calendar.ISO
 
-    @impl true
     defdelegate iso_days_to_end_of_day(iso_days), to: Calendar.ISO
   end
 end
